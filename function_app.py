211555e3ab5f6639d81d8ce7bd032b85913a35d2
ciribelli_petro <ciribelli@petrobras.com.br>
2025-05-05 13:50:13 -0300

    teste de escrita no log

--- function_app.py
+++ function_app.py
@@ -3,7 +3,7 @@ import azure.functions as func
 
 app = func.FunctionApp()
 
-@app.timer_trigger(schedule="0 0 8-20 * * *", arg_name="myTimer", run_on_startup=False,
+@app.timer_trigger(schedule="0 0 14-20 * * *", arg_name="myTimer", run_on_startup=False,
               use_monitor=False) 
 def etl_func(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
