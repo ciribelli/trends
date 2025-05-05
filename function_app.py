089ce321f5b9e8ab42c86b4d96892672d2c7c1c8
ciribelli_petro <ciribelli@petrobras.com.br>
2025-05-05 15:19:12 -0300

    rollback

--- function_app.py
+++ function_app.py
@@ -1,37 +1,48 @@
+# import logging
+# import requests
+# import azure.functions as func
+# import os
+
+# app = func.FunctionApp()
+
+# def send_wapp_msg(phone_number_id, from_number, coletor, wapp_token):
+#     fb_url = f"https://graph.facebook.com/v20.0/{phone_number_id}/messages?access_token={wapp_token}"
+#     payload = {
+#         "messaging_product": "whatsapp",
+#         "to": from_number,
+#         "text": {"body": coletor}
+#     }
+#     headers = {"Content-Type": "application/json"}
+#     response = requests.post(fb_url, json=payload, headers=headers)
+#     # print(response.content, "content")
+#     return response
+
+# @app.timer_trigger(schedule="0 0 14-20 * * *", arg_name="myTimer", run_on_startup=False,
+#               use_monitor=False) 
+# def etl_func(myTimer: func.TimerRequest) -> None:
+#     if myTimer.past_due:
+#         logging.info('Log de teste')
+
+#     wapp_token = os.environ.get('WHATSAPP_TOKEN')
+#     logging.info(wapp_token)
+#     logging.info('Rodando a aplicação!')
+#     a = send_wapp_msg("233405413182343","5521983163900","_mensagem teste api response pela Azure!_", wapp_token)
+#     logging.info(a.text)
+
+
+
+
 import logging
-import requests
 import azure.functions as func
-import os
-
+ 
 app = func.FunctionApp()
+ 
+@app.timer_trigger(schedule="0 0 8-20 * * *", arg_name="myTimer", run_on_startup=False, use_monitor=False) 
 
-def send_wapp_msg(phone_number_id, from_number, coletor, wapp_token):
-    fb_url = f"https://graph.facebook.com/v20.0/{phone_number_id}/messages?access_token={wapp_token}"
-    payload = {
-        "messaging_product": "whatsapp",
-        "to": from_number,
-        "text": {"body": coletor}
-    }
-    headers = {"Content-Type": "application/json"}
-    response = requests.post(fb_url, json=payload, headers=headers)
-    # print(response.content, "content")
-    return response
-
-@app.timer_trigger(schedule="0 0 14-20 * * *", arg_name="myTimer", run_on_startup=False,
-              use_monitor=False) 
 def etl_func(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
-        logging.info('Log de teste')
-
-    wapp_token = os.environ.get('WHATSAPP_TOKEN')
-    logging.info(wapp_token)
-    logging.info('Rodando a aplicação!')
-    a = send_wapp_msg("233405413182343","5521983163900","_mensagem teste api response pela Azure!_", wapp_token)
-    logging.info(a.text)
-
-
-
-
-
+        logging.info('The timer is past due!')
+ 
+    logging.info('Python timer trigger function executed.')
 
 
