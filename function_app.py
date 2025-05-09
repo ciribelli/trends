4985b50aadf8d9478621ab08c7b52f6e22380409
Otavio Ciribelli <tiotalgmail.com@MacBook-Pro-de-Otavio.local>
2025-05-09 16:30:13 -0300

    from mac

--- function_app.py
+++ function_app.py
@@ -10,7 +10,7 @@ def send_wapp_msg(phone_number_id, from_number, coletor, wapp_token):
     payload = {
         "messaging_product": "whatsapp",
         "to": from_number,
-        "text": {"body": "✅ Mensagem de teste enviada pela Azure Function!"}
+        "text": {"body": "✅ Mensagem de teste enviada pela Azure Function! Testando repositorio via Macbook"}
     }
     headers = {"Content-Type": "application/json"}
 
