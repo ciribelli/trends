4ab417b32ec901183e20ac36907a5416297c3811
Otavio <otavio.ciribelli@gmail.com>
2025-05-31 20:07:38 -0300

    testing two different schedules at same file

--- function_app.py
+++ function_app.py
@@ -31,3 +31,12 @@ def etl_func(myTimer: func.TimerRequest) -> None:
     logging.info('Executando envio de mensagem via WhatsApp...')
     resultado_busca = grok.consulta_grok(grok_token)
     send_wapp_msg("233405413182343","5521983163900",resultado_busca, wapp_token)
+
+
+# ⏲️ Timer a cada 30 minutos
+@app.timer_trigger(schedule="0 */15 * * * *", arg_name="verificacaoTimer", run_on_startup=False, use_monitor=False)
+def verificar_compromissos(verificacaoTimer: func.TimerRequest) -> None:
+    if verificacaoTimer.past_due:
+        logging.info('The timer is past due!')
+
+    logging.info('Executando verificação de compromissos no Heroku...')
