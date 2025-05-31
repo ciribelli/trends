e3329e69dfdd50780efe0c890d3d6975994bdda3
Otavio <otavio.ciribelli@gmail.com>
2025-05-31 20:31:58 -0300

    nova tentativa

--- function_app.py
+++ function_app.py
@@ -15,18 +15,31 @@ def send_wapp_msg(phone_number_id, from_number, coletor, wapp_token):
     }
     headers = {"Content-Type": "application/json"}
 
-    response = requests.post(url, json=payload, headers=headers)
+    try:
+        response = requests.post(url, json=payload, headers=headers)
+        response.raise_for_status()  # Levanta exceções para códigos de erro HTTP
+        logging.info(f"Status code: {response.status_code}")
+        logging.info(f"Response: {response.text}")
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Erro ao enviar mensagem: {e}")
 
-    logging.info(f"Status code: {response.status_code}")
-    logging.info(f"Response: {response.text}")
-
-@app.timer_trigger(schedule="0 0 9-9 * * *", arg_name="myTimer", run_on_startup=False, use_monitor=False)
+@app.timer_trigger(schedule="0 0 9 * * *", arg_name="myTimer", run_on_startup=False, use_monitor=False)
 def etl_func(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
-        logging.info('The timer is past due!')
+        logging.warning('The timer is past due!')
 
     wapp_token = os.getenv('WHATSAPP_TOKEN')
     grok_token = os.getenv('XAI_API_KEY')
-    logging.info('Executando envio de mensagem via WhatsApp...')
-    resultado_busca = grok.consulta_grok(grok_token)
-    send_wapp_msg("233405413182343","5521983163900",resultado_busca, wapp_token)
+
+    try:
+        resultado_busca = grok.consulta_grok(grok_token)
+        send_wapp_msg("233405413182343", "5521983163900", resultado_busca, wapp_token)
+    except Exception as e:
+        logging.error(f"Erro na execução do ETL: {e}")
+
+@app.timer_trigger(schedule="0 */30 * * * *", arg_name="verificacaoTimer", run_on_startup=False, use_monitor=False)
+def verificar_compromissos(verificacaoTimer: func.TimerRequest) -> None:
+    if verificacaoTimer.past_due:
+        logging.warning('The timer is past due!')
+    logging.info('Executando verificação de compromissos no Heroku...')
+    # Adicionar logica aqui
